633534c9f27bc592357a7fdb3faa984febaffb29
Shorov Kumer Saha <shorovkumersaha@Shorovs-MacBook-Air.local>
2025-12-06 12:24:15 -0800

    modifyfooter added x logo

--- src/compunents/Footer.jsx
+++ src/compunents/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
+import tlogo from '../assets/tlogo.jpeg'
 
 const Footer = () => {
   return (
 
-    <div>
+    <div className='mt-30'>
       <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
         <aside>
           <svg
@@ -30,16 +31,8 @@ const Footer = () => {
           <div>
             <h6 className="footer-title text-[20px]">Social</h6>
           <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current">
-                <path
-                  d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
+            <a href="https://x.com/settings/account" target='_blank' rel='noreferrer'>
+              <img className='w-8 h-7 rounded-full object-cover' src={tlogo} alt="" />
             </a>
             <a>
               <svg
